f0fa49b0832de4c1c4a3f8c7fa85beccf531929f
Aman Soni <89303443+Amansoni3@users.noreply.github.com>
2023-04-09 16:13:43 +0530

    movix app code completed

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import {
+  FaFacebookF,
+  FaInstagram,
+  FaTwitter,
+  FaLinkedin,
+} from "react-icons/fa";
+
+import ContentWrapper from "../contentWrapper/ContentWrapper";
+
+import "./style.scss";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <ContentWrapper>
+        <ul className="menuItems">
+          <li className="menuItem">Terms Of Use</li>
+          <li className="menuItem">Privacy-Policy</li>
+          <li className="menuItem">About</li>
+          <li className="menuItem">Blog</li>
+          <li className="menuItem">FAQ</li>
+        </ul>
+        <div className="infoText">
+          Thank you for using our movie rating project! Our mission is to provide you with accurate and reliable ratings for the movies you love. Our team of experts is dedicated to analyzing and evaluating each movie based on various criteria to give you the best possible rating. We hope our service has helped you discover new movies to enjoy and we look forward to continuing to provide you with the latest and most comprehensive movie ratings.
+        </div>
+        <div className="socialIcons">
+          <span className="icon">
+            <FaFacebookF />
+          </span>
+          <span className="icon">
+            <FaInstagram />
+          </span>
+          <span className="icon">
+            <FaTwitter />
+          </span>
+          <span className="icon">
+            <FaLinkedin />
+          </span>
+        </div>
+      </ContentWrapper>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/pages/404/PageNotFound.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import "./style.scss"
+
+const PageNotFound = () => {
+  return (
+    <div>Error</div>
+  )
+}
+export default PageNotFound
